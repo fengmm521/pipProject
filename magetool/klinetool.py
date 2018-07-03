4e7ff437c88f0845a9595a2a051596ebe208a60a
woodcol <fengmm521@gmail.com>
2018-07-03 21:51:06 +0800

    add klinetool

--- magetool/klinetool.py
+++ magetool/klinetool.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
+# @Date    : 2017-12-28 16:28:50
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
 
-def getAverageLineData(datas,pAver = 3,didx = 4):
+import os,sys
+from magetool import timetool
+import numpy as np
+
+#didx为数值在数据中的序号，用来取作均线计算的数据位
+def getAverageData(datas,pAver = 3,didx = 4):
     #[1517536380000,142.443,142.443,142,142,3486,244.8654307]
     ##时间戳，开，高，低，收，交易量，交易量转化为BTC或LTC数量
     outs = []
@@ -22,12 +30,83 @@ def getAverageLineData(datas,pAver = 3,didx = 4):
         outs.append(tmps)
     return outs
 
+def subList(lis1,lis2):
+    v1 = list(lis1)
+    v2 = list(lis2)
+    v = list(map(lambda x: x[0]-x[1], zip(v1, v2)))
+    return v
 
+
+def get_EMA(df,N,idx = 4):  
+    emas = []
+    for i in range(len(df)):  
+        if i==0:  
+            emas = [df[i][idx]]
+        if i>0:  
+            emastmp = (emas[-1] * (N-1.0) + df[i][idx] * 2.0)/(N+1.0)
+            emas.append(emastmp)
+    return emas  
+
+
+
+def get_MACD(df,dshort = 12,dlong = 26,M = 9):
+    a=get_EMA(df,dshort)  
+    b=get_EMA(df,dlong)  
+    DIF=subList(a, b) 
+
+    DEA = []
+    #print(df['diff'])  
+    for i in range(len(df)):  
+        if i==0:  
+            # df.ix[i,'dea']=df.ix[i,'diff'] 
+            DEA = [DIF[i]] 
+
+        if i>0:  
+            DEAtmp = (2 * DIF[i] + (M - 1) * DEA[i-1])/(M+1)
+            DEA.append(DEAtmp)
+
+    MACD = list(map(lambda x: (x[0]-x[1])*2, zip(DIF, DEA)))
+
+    return DIF,DEA,MACD
+    
+
+def test():
+    # openLong()
+    print timetool.getNowDate()
+    #1.buy,0.不操作，-1.sell
+
+
+
+def getLastMacdType(macd):
+
+    mtmps = macd[::-1]
+    SXidx = -1
+    JXidx = -1
+    for i in range(len(mtmps)):
+        if i > 0:
+            if macd[i-1] >= 0 and macd[i] < 0:
+                SXidx = i
+            if macd[i-1] <= 0 and macd[i] > 0:
+                JXidx = i
+
+            if SXidx >= 0 and JXidx >= 0:
+                break
+    return SXidx,JXidx
+
+
+def main():
+    # k5d = read5MimKline()
+    #['时间戳'，'开'，'高'，'低'，'收'，'交易量'，'交易量转化为BTC或LTC数量']
+    kdatas = []
+    # isUP = isClose(k1d)
+    if not kdatas:
+        return
+    dif,dea,macd = get_MACD(kdatas)
+
+
+    
+
+#测试
 if __name__ == '__main__':
-    # print datetime.datetime.utcnow()
-    # print timestamp_utc_now()
-    # print timestamp2datetime(int(time.time()),True)
-    tmpdat = '2017_7_17'
-    print(getDateDaysFromOneDate(tmpdat))
-    # outstr = timestamp2datetime(int(time.time() + 60 * 5),True)
-    # print outstr
+    main()
+    # test()
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "magetool",
-    version = "0.1.22",
+    version = "0.1.23",
     keywords = ("pip", "pathtool","timetool", "magetool", "mage"),
     description = "time and path tool",
     long_description = "time and path tool",
