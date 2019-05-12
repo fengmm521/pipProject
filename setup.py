b5f0c7af2656a17a9fd8c568d03f8d7e0ecb9d09
woodcol <fengmm521@gmail.com>
2019-05-12 18:17:09 +0800

    fix python3 print func

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "magetool",
-    version = "0.1.29",
+    version = "0.1.31",
     keywords = ("pathtool","timetool", "magetool", "mage"),
     description = "time and path tool",
     long_description = "time and path tool",
