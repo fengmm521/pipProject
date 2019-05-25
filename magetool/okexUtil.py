cce74a38c21677ba52b43136bd1b4f942fd24dac
woodcol <fengmm521@gmail.com>
2019-05-26 05:20:34 +0800

    add okexutil function

--- magetool/okexUtil.py
+++ magetool/okexUtil.py
@@ -10,14 +10,16 @@ import sympy
 
 
 
-def okexQP(kprice,dk,gg = 20.0,qplv = 0.2):
+
+#kprice:开仓价格,dk:开仓方向,gg:杠杆倍数,qplv:强平保证金率
+def okexQP(kprice,dk,num = 1.0,gg = 20.0,qplv = 0.2):
     if type(kprice) == float and kprice > 1.0 and (dk == 1 or dk == -1):
         kp = kprice         #开仓价
         mz = 100.0          #定单面值为100美元
-        kbzj = (mz/kp)/gg   #保证金数量
+        kbzj = num*(mz/kp)/gg   #保证金数量
         x = sympy.symbols('x')
         wsx = dk*(((mz/kp)*(x-kp))/x)/(mz/(kp*gg))*kbzj     #未实现盈亏
-        out = sympy.solve( ((kbzj + wsx)*kp*gg/(mz)) - qplv,x)    #计算结果
+        out = sympy.solve( ((kbzj + wsx)*kp*gg/(mz*num)) - qplv,x)    #计算结果
         print(out)
         print(type(out),len(out))
         pout = out[0]
@@ -28,11 +30,56 @@ def okexQP(kprice,dk,gg = 20.0,qplv = 0.2):
         return dicout,
     else:
         return {}
+
+
+#kplist:开仓价格和数量
+def okexQP_Mutil(kplist,dk,gg = 20.0,qplv = 0.20):
+    kbzj_all = 0.0   #保证金数量
+    mz = 100.0          #定单面值为100美元
+    # kp = kprice         #开仓价    
+    x = sympy.symbols('x')
+    wsx_all = 0.0
+    k_all = 0.0
+    num_all = 0.0
+    minprice = 0.0
+    maxprice = 0.0
+    for i,v in enumerate(kplist):
+        kp = v[0]
+        num = v[1]
+        kbzj = num*(mz/kp)/gg   #保证金数量
+        kbzj_all += kbzj
+        k_all += kp*num
+        num_all += num
+        wsx = dk*(((mz/kp)*(x-kp))/x)/(mz/(kp*gg))*kbzj     #未实现盈亏
+        wsx_all += wsx
+        if maxprice == 0.0 or kp > maxprice:
+            maxprice = kp
+        if minprice == 0.0 or kp < minprice:
+            minprice = kp
+    kp_all = k_all/num_all
+    out = sympy.solve( ((kbzj_all + wsx_all)*kp_all*gg/(mz*num_all)) - qplv,x)    #计算结果
+    print(out)
+    print(type(out),len(out))
+    pout = out[0]
+    shylv = dk*(((mz/kp_all)*(pout-kp_all))/pout)/(mz/(kp_all*gg))
+    wsx_out = shylv*kbzj_all
+    bzjv = 1.0 + shylv
+    outpercent = 0
+    if dk < 0:
+        outpercent = abs(pout - minprice)*100/minprice
+    elif dk > 0:
+        outpercent = abs(pout - maxprice)*100/maxprice
+    dicout = {'kbzj':kbzj_all,'qpPrice':pout,'wsx':wsx_out,'sylv':shylv,'bzjv':bzjv,'pprice':kp_all,'maxpercent':outpercent}
+    return dicout,
+
 def main(kprice,dk):
     out = okexQP(float(kprice),float(dk))
     print(out)
 def test():
-    out = okexQP(8148,1)
+    # out = okexQP(8148,1)
+    # print(out)
+    out = okexQP_Mutil([[7310.1,4],[7351.83,3],[7400.86,3],[7460,3],[7534,2],[7635.48,2],[7788,1],[8100,1]], 1)
+    # out = okexQP_Mutil([[7310.1,1],[7572.83,8]], 1)
     print(out)
 #测试
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "magetool",
-    version = "0.1.34",
+    version = "0.1.35",
     keywords = ("pathtool","timetool", "magetool", "mage"),
     description = "time and path tool",
     long_description = "time and path tool",
