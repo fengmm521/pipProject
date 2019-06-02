496f2e021862c1db8fa3ba29e2755996f317d7d6
woodcol <fengmm521@gmail.com>
2019-06-02 18:10:56 +0800

    add okexutil

--- magetool/okexUtil.py
+++ magetool/okexUtil.py
@@ -27,7 +27,7 @@ def okexQP(kprice,dk,num = 1.0,gg = 20.0,qplv = 0.2):
         wsx_out = shylv*kbzj
         bzjv = 1.0 + shylv
         dicout = {'kbzj':kbzj,'qpPrice':pout,'wsx':wsx_out,'sylv':shylv,'bzjv':bzjv}
-        return dicout,
+        return dicout
     else:
         return {}
 
@@ -58,8 +58,8 @@ def okexQP_Mutil(kplist,dk,gg = 20.0,qplv = 0.20):
             minprice = kp
     kp_all = k_all/num_all
     out = sympy.solve( ((kbzj_all + wsx_all)*kp_all*gg/(mz*num_all)) - qplv,x)    #计算结果
-    print(out)
-    print(type(out),len(out))
+    # print(out)
+    # print(type(out),len(out))
     pout = out[0]
     shylv = dk*(((mz/kp_all)*(pout-kp_all))/pout)/(mz/(kp_all*gg))
     wsx_out = shylv*kbzj_all
@@ -70,23 +70,75 @@ def okexQP_Mutil(kplist,dk,gg = 20.0,qplv = 0.20):
     elif dk > 0:
         outpercent = abs(pout - maxprice)*100/maxprice
     dicout = {'kbzj':kbzj_all,'qpPrice':pout,'wsx':wsx_out,'sylv':shylv,'bzjv':bzjv,'pprice':kp_all,'maxpercent':outpercent}
-    return dicout,
+    return dicout
+
+
+def addNextPrice(plists,dk):
+    dktype = 1
+    if dk < 0:
+        dktype = -1
+    minput = []
+    for i,v in enumerate(plists):
+        minput.append([v,1])
+    mtmp = okexQP_Mutil(minput, dktype)
+    plists.append(mtmp['qpPrice'])
+    return mtmp['kbzj'] #反回开仓保证金
+
+def mutilPrice(kprice,dp):
+    out = []
+    if dp > 1:
+        count = dp
+        tmp = okexQP(float(kprice),1)
+        # {'bzjv': 0.200000000000000, 'wsx': -0.000453001132502831, 'qpPrice': 8490.38461538462, 'sylv': -0.800000000000000, 'kbzj': 0.000566251415628539}
+        # print(tmp)
+        qpprice = tmp['qpPrice']
+        tmplist = [kprice]
+        tmplist.append(qpprice)
+        kbzj = 0.0 #保证金
+        count -= 1
+        while count >0:
+            kbzj = addNextPrice(tmplist,1)
+            count -= 1
+        # print(tmplist)
+        return tmplist,kbzj
+    elif dp < 0:
+        count = dp
+        tmp = okexQP(float(kprice),-1)
+        qpprice = tmp['qpPrice']
+        tmplist = [kprice]
+        tmplist.append(qpprice)
+        kbzj = 0.0 #保证金
+        count += 1
+        while count <0:
+            kbzj = addNextPrice(tmplist,-1)
+            count += 1
+        # print(tmplist)
+        return tmplist,kbzj #保证金
+    else:
+        return [],None
 
 def main(kprice,dk):
-    out = okexQP(float(kprice),float(dk))
-    print(out)
+    if dk > 1 or dk < -1:
+        out = mutilPrice(float(kprice),int(dk))
+        print(out)
+    else:
+        out = okexQP(float(kprice),int(dk))
+        print(out)
 def test():
     # out = okexQP(8148,1)
     # print(out)
-    out = okexQP_Mutil([[7310.1,4],[7351.83,3],[7400.86,3],[7460,3],[7534,2],[7635.48,2],[7788,1],[8100,1]], 1)
+    # out = okexQP_Mutil([[7310.1,4],[7351.83,3],[7400.86,3],[7460,3],[7534,2],[7635.48,2],[7788,1],[8100,1]], 1)
     # out = okexQP_Mutil([[7310.1,1],[7572.83,8]], 1)
+    
+    out = mutilPrice(8848.0, 3)
+
     print(out)
 #测试
 if __name__ == '__main__':
     args = sys.argv
     fpth = ''
     if len(args) == 3 :
-        main(args[1], args[2])
+        main(float(args[1]), int(args[2]))
     else:
         print "请加上开仓价，开仓方向，-1为空仓，1为多仓"
     # test()
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "magetool",
-    version = "0.1.35",
+    version = "0.1.36",
     keywords = ("pathtool","timetool", "magetool", "mage"),
     description = "time and path tool",
     long_description = "time and path tool",
