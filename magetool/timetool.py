cafcf68f9caaa6d1792309ca44d9ca4f5dbc04f4
woodcol <fengmm521@gmail.com>
2020-08-09 23:15:42 +0800

    增加时间戳转iso时间和ios转时间戳函数

--- magetool/timetool.py
+++ magetool/timetool.py
@@ -23,6 +23,50 @@ import sys
 # print "aa", time.time()
 # print time.localtime(time.time() + 60 *3)
 
+import pytz
+
+#iso转时间戳
+def iso2timestamp(datestring, format='%Y-%m-%dT%H:%M:%S.%fZ',timespec='seconds'):
+    """
+    ISO8601时间转换为时间戳
+
+    :param datestring:iso时间字符串 2019-03-25T16:00:00.000Z，2019-03-25T16:00:00.000111Z
+    :param format:%Y-%m-%dT%H:%M:%S.%fZ；其中%f 表示毫秒或者微秒
+    :param timespec:返回时间戳最小单位 seconds 秒，milliseconds 毫秒,microseconds 微秒
+    :return:时间戳 默认单位秒
+    """
+    tz = pytz.timezone('Asia/Shanghai')
+    utc_time = datetime.datetime.strptime(datestring, format)  # 将字符串读取为 时间 class datetime.datetime
+
+    time = utc_time.replace(tzinfo=pytz.utc).astimezone(tz)
+
+    times = {
+        'seconds': int(time.timestamp()),
+        'milliseconds': round(time.timestamp() * 1000),
+        'microseconds': round(time.timestamp() * 1000 * 1000),
+    }
+    return times[timespec]
+
+#时间戳转iso
+def timestamp2iso(timestamp, format='%Y-%m-%dT%H:%M:%S.%fZ'):
+    """
+    时间戳转换到ISO8601标准时间(支持微秒级输出 YYYY-MM-DD HH:MM:SS.mmmmmm)
+    :param timestamp:时间戳，支持 秒，毫秒，微秒级别
+    :param format:输出的时间格式  默认 iso=%Y-%m-%dT%H:%M:%S.%fZ；其中%f表示微秒6位长度
+
+    此函数特殊处理，毫秒/微秒部分 让其支持该部分的字符格式输出
+    :return:
+    """
+    format = format.replace('%f','{-FF-}')#订单处理微秒数据 %f
+    length = min(16, len(str(timestamp)))#最多去到微秒级
+
+    #获取毫秒/微秒 数据
+    sec = '0'
+    if length != 10:#非秒级
+        sec = str(timestamp)[:16][-(length - 10):]#最长截取16位长度 再取最后毫秒/微秒数据
+    sec = '{:0<6}'.format(sec)#长度位6，靠左剩下的用0补齐
+    timestamp = float(str(timestamp)[:10])#转换为秒级时间戳
+    return datetime.datetime.utcfromtimestamp(timestamp).strftime(format).replace('{-FF-}',sec)
 
 
 
